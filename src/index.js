ed1c2e01b6ecf8ef96e655d3faa2cac80e8059b0
verangade <verangades@gmail.com>
2021-10-13 21:41:39 +0800

    use list and loop

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ let city = {
 
 };
 
+const itemList = ['apple','banana','grapes'];
+
 function Lake(props){
   return <h1>{props.name}</h1>;
 
@@ -16,17 +18,26 @@ function Lake(props){
 function App(){
   return (
     <div>
-    <Lake name='hello how are you'/>
-    <Lake name='xxxx'/>
-    <Lake name='ppppp'/>
+      <Lake name='hello how are you'/>
+      <Lake name='xxxx'/>
+      <Lake name='ppppp'/>
     </div>
+    
   );
 
 }
 
+function App2(props){
+  return(
+    <div>
+      {props.items.map( item =>( <li>{item}</li>)) }
+    </div>
+  );
+}
+
 ReactDOM.render(
- <App/>
- ,
+ //<App/>
+ <App2 items={itemList}/>,
   document.getElementById('root')
 );
 
